551b82089e316f7c2231da6eaa3e04ef7a2fc246
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-04-23 16:59:10 +0530

    paraller_loop

---
+++ 09_cpp_loop/paraller_loop.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+    for(int i=1; i<=10; i++)
+    {
+        cout << i << " ";
+    }
+
+
+    cout << endl;
+
+    for(int j=10; j>0; j--)
+    {
+        cout << j << " ";
+    }
+
+
+
+    // while loop
+
+    cout << endl << "---------------" <<  endl;
+
+
+    int k=1;
+
+    while(k<10){
+
+        cout << k << " ";
+        k++;
+    }
+
+    cout << endl;
+
+    int l = 10;
+
+    while(l>0){
+        cout << l <<" ";
+        l--;
+    }
+
+    return 0;
+}
