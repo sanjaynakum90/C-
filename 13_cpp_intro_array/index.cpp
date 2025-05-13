dcc8631fd403950bb6e4bc550bb361c00939f10e
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-05-13 10:45:43 +0530

    intro of array

---
+++ 13_cpp_intro_array/index.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ 
+ using namespace std;
+ 
+ int main()
+ {
+     int n = 1;
+     int n2 =2;
+     int n3 = 3;
+ 
+     char el = 'a';
+ 
+     cout << sizeof(n)<< endl;
+     cout << sizeof(el) <<endl;
+ 
+    return 0;
+ }
